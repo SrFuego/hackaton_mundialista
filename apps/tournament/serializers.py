ddaa6403f7df318cb88e6d7cccd4667991097cf8
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-27 03:02:27 -0500

    ADD improve tournament serializer

--- apps/tournament/serializers.py
+++ apps/tournament/serializers.py
@@ -6,16 +6,27 @@
 
 
 # Third party apps imports
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 
 # Local imports
-from .models import Match
+from .models import Match, Stadium
+
+from ..geolocation.serializers import CountryModelSerializer
 
 
 # Create your serializers here.
-class MatchModelSerializer(ModelSerializer):
+class StadiumModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stadium
+        fields = ("id", "name", "city", "image",)
+
+
+class MatchModelSerializer(serializers.ModelSerializer):
+    country_1 = CountryModelSerializer()
+    country_2 = CountryModelSerializer()
+    stadium = StadiumModelSerializer()
 
     class Meta:
         model = Match
-        fields = ("country_1", "country_2", "date", "stadium", "status",)
+        fields = ("id", "country_1", "country_2", "date", "stadium", "status",)
