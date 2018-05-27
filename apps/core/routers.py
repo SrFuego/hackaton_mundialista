7298224c432107a0ea2564d9364885a16a2a1178
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-27 02:41:14 -0500

    ADD match un core routers

--- apps/core/routers.py
+++ apps/core/routers.py
@@ -9,9 +9,10 @@ from rest_framework.routers import DefaultRouter
 
 # Local imports
 from ..geolocation.routers import geolocalization
+from ..tournament.routers import tournament
 
 # Create your routers here.
-routers_tuples = (geolocalization,)
+routers_tuples = (geolocalization, tournament,)
 routers_lists = sum(
     [list(router_tuple) for router_tuple in routers_tuples], [])
 
